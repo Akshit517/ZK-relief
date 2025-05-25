ff5a4c6885ff34dac139162a2d65315adc303624
nitininhouse <nitin_g@me.iitr.ac.in>
2025-05-26 01:07:57 +0530

    chnaged integeration

--- frontend/zk-relief/app/src/app/auth/page.tsx
+++ frontend/zk-relief/app/src/app/auth/page.tsx
@@ -306,11 +306,7 @@ export default function Page() {
 
     return (
         <div id="cb" className="space-y-12">
-            <div className="px-4 sm:px-0">
-                <h3 className="text-base font-semibold leading-7 text-gray-900">Authenticated Area</h3>
-                <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Login with External Provider Completed</p>
-            </div>
-
+           
 
             <div className="mt-6 border-t border-gray-100">
                 <dl className="divide-y divide-gray-100">
